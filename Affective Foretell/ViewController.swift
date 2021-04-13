0f94d90cff08f909e4988e8f426f1f0e703ad379
tianyicchenn <tianyichen9828@gmail.com>
2021-04-13 23:28:27 +0200

    Enable basic face tracking function

--- Affective Foretell/ViewController.swift
+++ Affective Foretell/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var faceView: ARSCNView!
+    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        faceView.delegate = self
+        guard ARFaceTrackingConfiguration.isSupported else {
+            statusLabel.text = "AR Face Detection is not supported"
+            return
+        }
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARFaceTrackingConfiguration()
+        faceView.session.run(configuration)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        faceView.session.pause()
+    }
+    
+    
+    
 
 }
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = faceView.device else { return nil }
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let faceNode = SCNNode(geometry: faceGeometry)
+        faceNode.geometry?.firstMaterial?.fillMode = .lines
+        return faceNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAncher = anchor as? ARFaceAnchor,
+        let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+            return
+        }
+        faceGeometry.update(from: faceAncher.geometry)
+    }
+}
